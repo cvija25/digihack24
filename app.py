de78ceafa4625fc87752e658918bd2a81aacb3ac
cvija25 <lcvijicc@gmail.com>
2024-09-29 11:37:02 +0200

    app major

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import subprocess
 import numpy as np
+from numpy.linalg import norm
+from numpy import cross, dot
 import time
 
 def check_wifi_signal(ssids):
@@ -26,22 +28,47 @@ def check_wifi_signal(ssids):
 
 def get_distance(signal_strength):
     # Example function to convert signal strength (RSSI) to approximate distance
-    return 10 - signal_strength / 10
+    # return 10 - signal_strength / 10
+    return 10**(((25-signal_strength))/(10*3))*100
 
-def trilaterate(pointA, d1, pointB, d2, pointC, d3):
-    x1, y1 = pointA
-    x2, y2 = pointB
-    x3, y3 = pointC
+# def trilaterate(pointA, d1, pointB, d2, pointC, d3):
+#     x1, y1 = pointA
+#     x2, y2 = pointB
+#     x3, y3 = pointC
 
-    # Use equations derived from the distance formulas
-    A = np.array([[2*(x2-x1), 2*(y2-y1)], [2*(x3-x1), 2*(y3-y1)]])
-    B = np.array([[d1**2 -d2**2 -x1**2+x2**2-y1**2+y2**2], [d1**2 -d3**2 -x1**2+x3**2-y1**2+y3**2]])
+#     # Use equations derived from the distance formulas
+#     A = np.array([[2*(x2-x1), 2*(y2-y1)], [2*(x3-x1), 2*(y3-y1)]])
+#     B = np.array([[d1**2 -d2**2 -x1**2+x2**2-y1**2+y2**2], [d1**2 -d3**2 -x1**2+x3**2-y1**2+y3**2]])
 
-    return np.linalg.solve(A,B)
+#     return np.linalg.solve(A,B)
+
+def trilaterate(P1,r1,P2,r2,P3,r3):    
+    P1 = np.array(P1)                  
+    P2 = np.array(P2)                  
+    P3 = np.array(P3)                  
+    temp1 = P2-P1                                        
+    e_x = temp1/norm(temp1)                              
+    temp2 = P3-P1                                        
+    i = dot(e_x,temp2)                                   
+    temp3 = temp2 - i*e_x                                
+    e_y = temp3/norm(temp3)                              
+    e_z = cross(e_x,e_y)                                 
+    d = norm(P2-P1)                                      
+    j = dot(e_y,temp2)                                   
+    x = (r1*r1 - r2*r2 + d*d) / (2*d)                    
+    y = (r1*r1 - r3*r3 -2*i*x + i*i + j*j) / (2*j)       
+    temp4 = r1*r1 - x*x - y*y                            
+    if temp4<0:                                          
+        raise Exception("The three spheres do not intersect!")
+    z = np.sqrt(temp4)                                      
+    p_12_a = P1 + x*e_x + y*e_y + z*e_z                  
+    p_12_b = P1 + x*e_x + y*e_y - z*e_z                  
+    return p_12_a, p_12_b 
+
 
 if __name__ == "__main__":
     # Access point locations and SSIDs
-    ap_locs = [(7.5, 6), (12.5, 7), (8.75, 0)]
+    ap_locs = [(0,0), (0, 4), (4.5, 4)]
     ap_ssids = ["djole", "Bogdan's Galaxy S21 FE 5G", "Cvija iPhone"]
     
     # Check for Wi-Fi signals and get RSSI values
@@ -51,7 +78,7 @@ if __name__ == "__main__":
         # Map signal strengths to distances
         d1 = get_distance(signal_list[0])
         d2 = get_distance(signal_list[1])
-        d3 = get_distance(signal_list[2])
+        d3 = get_distance(signal_list[2]+10)
     
         print("Signal strengths:", signal_list)
         print("Distances:", d1, d2, d3)
